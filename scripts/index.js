3781b26c31921f2e4db7fe2e85606425ee775a76
Emiliano Aceves Hernández <0223332@up.edu.mx>
2021-10-26 13:40:59 -0500

    Subir los archivos
    Todos copiados directo del sandbox

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCF1gDRLZHkvErXcpCwsGqI_8F-nz2eYVE",
+  authDomain: "fazt-crud-fb-92214.firebaseapp.com",
+  projectId: "fazt-crud-fb-92214",
+  storageBucket: "fazt-crud-fb-92214.appspot.com",
+  messagingSenderId: "1030108619202",
+  appId: "1:1030108619202:web:c76afabf58d4cf5a5a765b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
